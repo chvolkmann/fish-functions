0cb2e2f92a7c4b19ca5a67621bd29445032bdc81
Christian Volkmann <ch.volkmann@gmail.com>
2023-05-20 15:25:25 +0200

    Add aliases

---
+++ conf.d/aliases-chvl.fish
@@ -0,0 +1,10 @@
+alias apt="sudo apt"
+alias apti="sudo apt install"
+alias aptu="sudo apt update"
+alias aptuu="sudo apt update && sudo apt upgrade"
+
+alias systemctl="sudo systemctl"
+alias journalctl="sudo journalctl"
+
+alias dc="docker compose"
+alias dk="docker"
